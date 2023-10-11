eee460c8a90806a92702385c44e80408e7c4af37
Pedro Pereira <palex@cc.isel.ipl.pt>
2023-10-11 13:13:41 +0100

    Aula 11 Outubro (Parte 2)

--- Ball/src/main/kotlin/Main.kt
+++ Ball/src/main/kotlin/Main.kt
@@ -2,37 +2,56 @@ import pt.isel.canvas.*
 
 const val WIDTH = 400
 const val HEIGHT = 400
-const val BALL_RADIUS = 100
+const val BALL_RADIUS = 50
 const val BALL_DELTA = 15
 const val MIN_X = BALL_RADIUS
 const val MAX_X = WIDTH - BALL_RADIUS
 const val MIN_Y = BALL_RADIUS
 const val MAX_Y = HEIGHT - BALL_RADIUS
+const val FRAME_TIME = 20 // milliseconds
+const val MAX_VELOCITY = 5
 
 data class Point(val x:Int, val y:Int)
 
+data class Ball(val center: Point, val dx: Int, val dy: Int)
+
 fun main() {
     onStart {
-        var center = Point(WIDTH/2,HEIGHT/2)
+        var ball = Ball(Point(WIDTH/2,HEIGHT/2),0,0)
         val cv = Canvas(WIDTH,HEIGHT, WHITE)
-        cv.drawBall(center)
-        cv.onKeyPressed { key ->
-            center = moveBall(key.code, center)
-            cv.drawBall(center)
+        cv.drawBall(ball)
+        cv.onKeyPressed { ke ->
+            ball = processKey(ke, ball)
+            cv.drawBall(ball)
+        }
+        cv.onMouseDown { me ->
+            ball = Ball(Point(me.x,me.y).coerce(),0,0)
+            cv.drawBall(ball)
+        }
+        cv.onTimeProgress(FRAME_TIME) {
+            ball = moveBall(ball)
+            cv.drawBall(ball)
         }
     }
     onFinish {  }
 }
 
-private fun moveBall(key: Int, b: Point): Point = inLimits( moveByKey(key, b) )
+private fun moveBall(ball: Ball) = Ball(
+    Point(ball.center.x + ball.dx, ball.center.y + ball.dy),
+    ball.dx, ball.dy
+)
 
-private fun inLimits(p: Point): Point = when {
-    p.x < MIN_X -> Point(MIN_X, p.y)
-    p.x > MAX_X -> Point(MAX_X, p.y)
-    p.y < MIN_Y -> Point(p.x, MIN_Y)
-    p.y > MAX_Y -> Point(p.x, MAX_Y)
-    else -> p
-}
+private fun processKey(ke: KeyEvent, ball: Ball) =
+    if (ke.code == 'G'.code)
+        Ball(ball.center,
+            dx = (-MAX_VELOCITY..MAX_VELOCITY).random(),
+            dy = (-MAX_VELOCITY..MAX_VELOCITY).random()
+        )
+    else
+        Ball(moveByKey(ke.code, ball.center).coerce(), 0, 0)
+
+private fun Point.coerce() =
+    Point( x.coerceIn(MIN_X..MAX_X), y.coerceIn(MIN_Y..MAX_Y))
 
 private fun moveByKey(key: Int, b: Point): Point = when (key) {
     LEFT_CODE -> Point(b.x - BALL_DELTA, b.y)
@@ -41,21 +60,13 @@ private fun moveByKey(key: Int, b: Point): Point = when (key) {
     DOWN_CODE -> Point(b.x, b.y + BALL_DELTA)
     else -> b
 }
-/*
-    when (key) {
-    LEFT_CODE ->
-        Point(if (b.x - BALL_DELTA >= MIN_X) b.x - BALL_DELTA else MIN_X, b.y)
-    RIGHT_CODE ->
-        Point(if (b.x + BALL_DELTA <= MAX_X) b.x + BALL_DELTA else MAX_X, b.y)
-    UP_CODE ->
-        Point(b.x, if (b.y - BALL_DELTA >= MIN_Y) b.y - BALL_DELTA else MIN_Y)
-    DOWN_CODE ->
-        Point(b.x, if (b.y + BALL_DELTA <= MAX_Y) b.y + BALL_DELTA else MAX_Y)
-    else -> b
-}
-*/
 
-private fun Canvas.drawBall(ball: Point) {
+private const val VECTOR_FACTOR = 8
+
+private fun Canvas.drawBall(ball: Ball) {
     erase()
-    drawCircle(ball.x, ball.y, BALL_RADIUS, GREEN)
+    val x = ball.center.x
+    val y = ball.center.y
+    drawCircle(x, y, BALL_RADIUS, GREEN)
+    drawLine(x,y, x+ball.dx* VECTOR_FACTOR, y+ball.dy* VECTOR_FACTOR, RED,2)
 }
