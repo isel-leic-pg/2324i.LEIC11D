afafa46fa0b2f43091167f9445dd3214ee100342
Pedro Pereira <palex@cc.isel.ipl.pt>
2023-11-15 21:44:35 +0000

    Aula 15 Novembro

--- euromilhoes/src/main/kotlin/Bet.kt
+++ euromilhoes/src/main/kotlin/Bet.kt
@@ -1,11 +1,13 @@
 
-const val MAX_NUMBER = 50   // Numbers in [1..50]
-const val MAX_STAR = 12     // Stars in [1..12]
-const val MIN_NUMBERS = 5   // Minimum numbers in a bet
-const val MIN_STARS = 2     // Minimum stars in a bet
+const val MAX_NUMBER = 50   // Numbers in 1..50
+const val MAX_STAR = 12     // Stars in 1..12
+const val MIN_NUMBERS = 5   // Minimum numbers in a simple bet
+const val MAX_NUMBERS = 11  // Maximum numbers in a multiple bet
+const val MIN_STARS = 2     // Minimum stars in a simple bet
+const val MAX_STARS = 12    // Maximum stars in a multiple bet
 
-val allNumbers = (1..MAX_NUMBER)
-val allStars = (1..MAX_STAR)
+val allNumbers = 1..MAX_NUMBER
+val allStars = 1..MAX_STAR
 
 /**
  * Represents a bet in the Euromilhões game.
@@ -13,24 +15,101 @@ val allStars = (1..MAX_STAR)
  * @property stars the stars of the bet.
  */
 data class Bet(
-    val numbers: List<Int>, // size in [5..50]
-    val stars: List<Int>,   // size in [2..12]
+    val numbers: List<Int>, // size in 5..11
+    val stars: List<Int>,   // size in 2..12
 )
 
 /**
  * Creates a random simple bet (5 numbers + 2 stars).
- * numbers in [1..50] and stars in [1..12] without repetitions.
+ * numbers in 1..50 and stars in 1..12 without repetitions.
  * @return the random bet.
  */
-fun createRandomBet() = Bet(
-    numbers = allNumbers.shuffled().take(MIN_NUMBERS),
-    stars = allStars.shuffled().take(MIN_STARS)
+fun createRandomSimpleBet() = Bet(
+    numbers = allNumbers.shuffled().take(MIN_NUMBERS).sorted(),
+    stars = allStars.shuffled().take(MIN_STARS).sorted()
 )
 
 /**
- * Show two randoms bets (only for debug).
+ * Reads a list of numbers, separated by space, from the console.
+ * Ignore words that are not numbers.
+ * The list returned is sorted.
  */
+fun readValues(quest: String): List<Int> {
+    print("$quest ? ")
+    return readln()
+        .split(' ')
+        .mapNotNull { word -> word.toIntOrNull() }
+        .sorted()
+}
+
+/**
+ * Reads a bet from the console.
+ * The bet may be invalid.
+ */
+fun readBet() = Bet(
+    readValues("Números"),
+    readValues("Estrelas")
+)
+
+/**
+ * Checks if all the values are in the range [1..max].
+ */
+fun List<Int>.valuesIn(max: Int) = all { it in 1..max }
+/*  forEach { value ->     //for(value in this) {
+        if (value !in 1..max) return false
+    }
+    return true
+*/
+
+/**
+ * Checks if the bet is valid:
+ * - all the numbers are in  1..MAX_NUMBER
+ * - all the stars are in 1..MAX_STAR
+ * - size of numbers is in MIN_NUMBERS..MAX_NUMBERS
+ * - size of stars is in MIN_STARS..MAX_STARS
+ * - there are no repetitions in numbers and stars
+ */
+fun Bet.isValid(): Boolean =
+    numbers.valuesIn(MAX_NUMBER) && stars.valuesIn(MAX_STAR) &&
+    numbers.size in MIN_NUMBERS..MAX_NUMBERS && stars.size in MIN_STARS..MAX_STARS &&
+    numbers.noRepeats() && stars.noRepeats()
+
+/**
+ * Checks if the list has no repetitions.
+ */
+fun List<Int>.noRepeats(): Boolean = distinct().size == size
+/*  for (v in this) {
+        if (v in (this - v)) return false
+    }
+    return true
+*/
+/*  for(idx in 0..size-2) {
+        val v = this[idx]
+        for(idx2 in idx+1..<size)
+            if (this[idx2]==v) return false
+    }
+    return true
+*/
+
+/**
+ * Reads a valid bet from the console.
+ * Repeat the read until the bet is valid.
+ */
+fun readValidBet(): Bet {
+    while(true) {
+        val bet = readBet()
+        if (bet.isValid()) return bet
+        println("Invalid bet")
+    }
+/*  var bet: Bet
+    do {
+        bet = readBet()
+    } while( ! bet.isValid() )
+    return bet  */
+}
+
 fun main() {
-    println( createRandomBet() )
-    println( createRandomBet() )
+    //println( listOf(3,5,9,5,2).noRepeats() )
+    repeat(10) { println(createRandomSimpleBet()) }
+    println(readValidBet())
 }
